cef9f6d3e0678ca3178b2b90dca73e88da968fc8
Mathamind7 <parthn254@gmail.com>
2023-07-23 10:50:59 +0530

    Final changes made

--- DEVSOC LEVEL DESIGN/Assets/Scripts/finish.cs
+++ DEVSOC LEVEL DESIGN/Assets/Scripts/finish.cs
@@ -7,7 +7,7 @@ public class finish : MonoBehaviour
 {
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		if(collision.gameObject.name == "Player")
+		if(collision.gameObject.tag == "Player")
 		{
 			completeLevel();
 		}
